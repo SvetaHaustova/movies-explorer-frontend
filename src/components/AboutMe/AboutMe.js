73be478fa91bf08feed963d1cadf6b23655c1ae0
Sveta Haustova <sveta.suprun.hsi@yandex.ru>
2022-02-28 12:31:29 +0300

    fix: change about me and portfolio

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,14 +8,18 @@ function AboutMe() {
             <div className="about-me__info">
                 <div className="about-me__description">
                     <p className="about-me__name">Светлана</p>
-                    <p className="about-me__profession">Фронтенд-разработчик, 32 года</p>
-                    <p className="about-me__text">Информация обо мне</p>
+                    <p className="about-me__profession">Фронтенд-разработчик, 33 года</p>
+                    <p className="about-me__text">
+                        Закончила курсы по веб-разработке в Яндекс.Практикуме, сейчас прохожу курс на React-разработчика. 
+                        Работала сначала в системе менеджмента качества, потом в бизнес-аналитике. В какой-то момент решила 
+                        попробовать свои силы в веб-разработке, и мне очень понравилось. Хочу и дальше развиваться в данном направлении.
+                    </p>
                     <ul className="about-me__list">
                         <li>
-                            <a className="about-me__link" href="https://telegram.org" target="_blank" rel="noreferrer">Telegram</a>
+                            <a className="about-me__link" href="https://t.me/svetllanasi" target="_blank" rel="noreferrer">Telegram</a>
                         </li>
                         <li>
-                            <a className="about-me__link" href="https://github.com/" target="_blank" rel="noreferrer">Github</a>
+                            <a className="about-me__link" href="https://github.com/svetlanassi" target="_blank" rel="noreferrer">Github</a>
                         </li>
                     </ul>
                 </div>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,13 +6,13 @@ function Portfolio() {
             <h2 className="portfolio__title">Портфолио</h2>
             <ul className="portfolio__list">
                 <li className="portfolio__item">
-                    <a className="portfolio__link" href="https://github.com/SvetaHaustova/how-to-learn" target="_blank" rel="noreferrer">
+                    <a className="portfolio__link" href="https://github.com/svetlanassi/how-to-learn" target="_blank" rel="noreferrer">
                         <p className="portfolio__link-text">Статичный сайт</p>
                         <p className="portfolio__link-text">&#8599;</p>
                     </a>
                 </li>
                 <li className="portfolio__item">
-                    <a  className="portfolio__link" href="https://svetahaustova.github.io/russian-travel/" target="_blank" rel="noreferrer">
+                    <a  className="portfolio__link" href="https://svetlanassi.github.io/russian-travel/" target="_blank" rel="noreferrer">
                         <p className="portfolio__link-text">Адаптивный сайт</p>
                         <p className="portfolio__link-text">&#8599;</p>
                     </a>
